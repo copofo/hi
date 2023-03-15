68ac887cbf0cd1b6da41e4ba9a8ffc919f6c85fb
copofo <copofo4243@gmail.com>
2023-03-15 14:01:43 -0300

    t

---
+++ script.js
@@ -0,0 +1,20 @@
+const lat = document.getElementById("lat")
+const lon = document.getElementById("lon")
+
+  
+ 
+
+
+
+if(navigator.geolocation){
+ navigator.geolocation.getCurrentPosition(mostrar)
+
+}else{
+ alert("localização desligada!")
+
+}
+
+function mostrar(pos){
+ console.log(`Latitude: ${pos.coords.latitude} Lingitude: ${ pos.coords.longitude} `)
+
+}
